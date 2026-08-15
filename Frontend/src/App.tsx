25236b80a38caf9ca6331c3e1decf208ef56fa55
Alexandru <alexandru.nic14@gmail.com>
2026-08-15 18:04:47 +0100

    Create App Foundation Layout

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,29 @@
+// Modules //
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import "./App.css";
+
+// Layouts //
+import MainLayout from "./layouts/MainLayout";
+
+// Routes //
+import Home from "./components/home/Home.tsx";
 
 function App() {
 
   return (
-    <div>Example Site</div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+        
+          {/* Landing Page */}
+          <Route path={"/"} element={<Navigate to="home" replace />} />
+          <Route path={"home"} element={<Home />} />
+
+          {/* Secondary Pages */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ Frontend/src/components/home/Home.tsx
@@ -0,0 +1,9 @@
+import "./Home.css";
+
+export default function Home() {
+    return (
+        <div>Example</div>
+    )
+};
+
+// Next: Finish designing home paage
