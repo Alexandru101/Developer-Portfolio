d4223b49d58f37b6dc1a858870a9dbf8dd11b39c
Alexandru <alexandru.nic14@gmail.com>
2026-08-19 16:59:28 +0100

    Refractor Code + Complete Landing Page First View Stylying + Mobile Compatibility

--- Frontend/src/components/home/Home.tsx
+++ Frontend/src/components/home/Home.tsx
@@ -1,17 +1,21 @@
 // Modules //
+import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 
 // Images + Icons //
 import avatar from "../../assets/avatar.png";
-import { LuLayers3, LuBriefcaseBusiness, LuUserRound, LuMail } from "react-icons/lu";
+import { LuLayers3, LuBriefcaseBusiness, LuUserRound, LuMail, LuMouse } from "react-icons/lu";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { BiChevronDown } from "react-icons/bi";
 
 // Rendering Page //
 export default function Home() {
     return (
         <div className={styles.page}>
-            <div className={styles.profile}>
+            <header className={styles.profile}>
                 <img
                     src={avatar}
+                    alt=""
                     className={styles.avatar}
                 />
 
@@ -19,40 +23,58 @@ export default function Home() {
                 <p className={styles.description}>
                     Full-Stack developer building modern web applications
                 </p>
-            </div>
+            </header>
 
             <nav className={styles.navigation}>
                 <span className={styles.glowLine} />
 
                 <div className={styles.navigation__options}>
-                    <a className={styles["navigation__options-card"]}>
+                    <Link to="/projects" className={styles.navigation__card}>
                         <span className={styles.card__icon}>{"</>"}</span>
                         <span className={styles.card__text}>Projects</span>
-                    </a>
+                    </Link>
 
-                    <a className={styles["navigation__options-card"]}>
+                    <Link to="/skills" className={styles.navigation__card}>
                         <LuLayers3 className={styles.card__icon}/>
                         <span className={styles.card__text}>Skills</span>
-                    </a>
+                    </Link>
 
-                    <a className={styles["navigation__options-card"]}>
+                    <Link to="/experience" className={styles.navigation__card}>
                         <LuBriefcaseBusiness className={styles.card__icon} />
                         <span className={styles.card__text}>Experience</span>
-                    </a>
+                    </Link>
 
-                    <a className={styles["navigation__options-card"]}>
+                    <Link to="/about" className={styles.navigation__card}>
                         <LuUserRound className={styles.card__icon} />
                         <span className={styles.card__text}>About</span>
-                    </a>
+                    </Link>
 
-                    <a className={styles["navigation__options-card"]}>
+                    <Link to="/contact" className={styles.navigation__card}>
                         <LuMail className={styles.card__icon} />
                         <span className={styles.card__text}>Contact</span>
+                    </Link>
+                </div>
+
+                <div className={styles.socials}>
+                    <a href="https://github.com/Alexandru101" aria-label="Github" >
+                        <FaGithub className={styles.socials__icon} />
+                    </a>
+
+                    <a href="https://gb.linkedin.com/">
+                        <FaLinkedin className={styles.socials__icon} />
+                    </a>
+
+                    <a href="https://x.com/?lang=en-gb">
+                        <FaTwitter className={styles.socials__icon} />
                     </a>
                 </div>
             </nav>
+
+            <div className={styles.scroll}>
+                <span className={styles.scroll__text}>Scroll</span>
+                <LuMouse size={30} strokeWidth={1} />
+                <BiChevronDown size={30} />
+            </div>
         </div>
     )
 };
-
-// Next: Finish landing page about me and scroll down design
